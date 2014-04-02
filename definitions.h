75dbc6b56d6d8e7d3f94f994b7b167f728ceedca
josanuz <josanuz@hotmail.es>
2014-04-01 21:44:53 -0600

    Mod Definitions

--- definitions.h
+++ definitions.h
@@ -4,3 +4,21 @@
 typedef unsigned long size;
 typedef unsigned long id;
 
+enum Tipo_Dato {
+	INTEGER,
+	DOUBLE,
+	STRING,
+	BYTE
+};
+int sizeDT(Tipo_Dato t){
+		switch(t){
+			case Tipo_Dato::BYTE:
+				return sizeof(char);
+			case Tipo_Dato::DOUBLE:
+				return sizeof(float);
+			case Tipo_Dato::INTEGER:
+				return sizeof(int);
+			case Tipo_Dato::STRING;
+				return sizeof(char[128]);
+		}
+}
