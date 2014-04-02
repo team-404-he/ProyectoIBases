6a7575099d74f785c46f07c84f76f6087f1332d3
asvaaron <asvaaron@gmail.com>
2014-04-01 21:38:36 -0600

    aqui esta la tabla

--- table.cpp
+++ table.cpp
@@ -7,4 +7,31 @@ Table::Table()
 Table::~Table()
 {
 }
+const Table::id GetID() const{
+		return this->ID;
+} 
+void Table::SetID(id _ID){
+	this->ID=_ID;
+}	
+const size& Table:: GetTamano() const{
+	return this->tamano;
+} 
+void Table::SetTamano(const size& tamano){
+	this->tamano=tamano;
+}
+const size& Table:: GetDireccionArchivo(){
+	return this->direcionEnArchivo;
+}
+void Table::SetDireccionArchivo(const size& Tam){
+	this->direcionEnArchivo;=tam;
+}
+const string& Table::GetNombre(){
+	return this->nombre;
+}
+void Table:: SetNombre(const string& nombre){
+	this->nombre=nombre;
+}
 
+list Table::GetColumnas(){
+	return this->columnas;
+}
--- table.h
+++ table.h
@@ -1,15 +1,25 @@
 #ifndef Table
 #define Table
 #include <list>
-
-class Table
-{
+using namespace std; 
+class Table{
 private:
 	list<Columna*> columnas;
+	id ID;
+	size tamano;
+	size direcionEnArchivo;
+	string nombre;
 public:
 	Table();
 	~Table();
-
+	const id GetID() const;
+	void SetID(id _ID);
+	const size& GetTamano() const ; 
+	void SetTamano(const size& tamano);
+	const size& GetDireccionArchivo();
+	void SetDireccionArchivo(const size& Tam);
+	const string& GetNombre();
+	void SetNombre(const string& nombre);
 };
 
 #endif // Table
