19ae973f83240dda2da1d34e46dd69d5a59d4c33
josanuz <josanuz@hotmail.es>
2014-04-01 23:57:07 -0600

    TableSpace

--- table_space.cpp
+++ table_space.cpp
@@ -2,9 +2,70 @@
 
 TableSpace::TableSpace()
 {
+	this->ID = 0;
+	this->nombre = "";
+	this->tamano = 0l
 }
 
 TableSpace::~TableSpace()
 {
 }
+TableSpace::TableSpace(id _id, string nombre):ID(_id), nombre(_nombre){
+		this->tamano = 0;
+}
+void TableSpace::addTable(Table * t){
+	this->tablas.push_back(t);
+}
+void TableSpace::removeTable(id){
+	typedef list<Table*>::iterator iter;
+	for(iter i = this->tablas.begin(); i != tablas.end() ; i++){
+		if( id == (*i)->id){
+			tablas.erase(i)
+			return;
+		}
+}
+void TableSpace::removeTable(Table *){
+			typedef list<Table*>::iterator iter;
+	for(iter i = this->tablas.begin(); i != tablas.end() ; i++){
+		if(id == (*i)){
+			tablas.erase(i)
+			return;
+		}
+}
+void TableSpace::serializate_this(ofstream& os){
+	
+	typedef list<Table*>::iterator iter;	
+	SerializadorBinario::serialize(os,this->ID);
+	SerializadorBinario::serialize(os,this->nombre);
+	SerializadorBinario::serialize(os,this->tamano);
+	SerializadorBinario::serialize(os,(long)this->tablas->size());
+		
+	for(iter i = this->tablas.begin(); i != tablas.end() ; i++){
+		(*i)->serializate_this(os);
+}
+TableSpace* TableSpace::deserialize_a_TableSpace(ifstream& is){
+		TableSpace * t = new TableSpace();
+		t->SetID(SerializadorBinario::deserializeLong(is));
+		t->SetNombre(SerializadorBinario::deserializeString(is));
+		t->SetTamano(SerializadorBinario::deserializeLong(is));
+		long x = SerializadorBinario::deserializeInt(is);
+		iter i = this->tablas.begin();
+		for(long h = 0; h < x ; h++){
+			this->addTable(Table::deserialize_a_table(is));
+		}
+		return t;
+}
 
+void TableSpace::SetID(const id& ID) 
+{this->ID = ID;}
+void TableSpace::SetNombre(const string& nombre)
+ {this->nombre = nombre;}
+void TableSpace::SetTamano(const size& tamano) 
+{this->tamano = tamano;}
+const id& TableSpace::GetID() const 
+{return ID;}
+const string& TableSpace::GetNombre() const 
+{return nombre;}
+const size& TableSpace::GetTamano() const {
+	return this->tamano;
+}
--- table_space.h
+++ table_space.h
@@ -1,14 +1,35 @@
 #ifndef TABLESPACE_H
 #define TABLESPACE_H
 
+#include "definitions.h"
+#include "table.h"
+#include "SerializadorBinario.h"
 #include <list>
+using namespace std;
 
 class TableSpace
 {
-	list<Tabla*> tablas;
+	
 public:
 	TableSpace();
-private:
+	TableSpace(id _id, string _nombre);
+	void addTable(Table *);
+	void removeTable(id);
+	void removeTable(Table *);
+	void serializate_this(ofstream&);
+	static TableSpace* deserialize_a_TableSpace(ifstream&);
+	
+	void SetID(const id& ID);
+	void SetNombre(const string& nombre);
+	void SetTamano(const size& tamano);
+	const id& GetID() const;
+	const string& GetNombre() const;
+	const size& GetTamano() const;
+	id ID;
+	string nombre;
+	size tamano;
+	std::list<Tabla*> tablas;
+
 	~TableSpace();
 
 };
